1e167c053eb71b2393a81d55c9aed7c5f8cfcd76
IvanGerasymenko <gerasymenko15@gmail.com>
2021-11-28 21:53:47 +0200

    Changed SubjectGroupEntity

--- src/main/java/com/example/facSchedule/entity/SubjectGroupEntity.java
+++ src/main/java/com/example/facSchedule/entity/SubjectGroupEntity.java
@@ -19,6 +19,11 @@ public class SubjectGroupEntity {
     @JoinColumn(name = "subject_id")
     private SubjectEntity subject;
 
+    /*
+    {
+      "groupName":"",
+    }
+*/
     @ManyToMany
     private List<StudentEntity> students;
 
@@ -47,4 +52,35 @@ public class SubjectGroupEntity {
         this.groupName = groupName;
     }
 
+    public ProfessorEntity getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(ProfessorEntity professor) {
+        this.professor = professor;
+    }
+
+    public SubjectEntity getSubject() {
+        return subject;
+    }
+
+    public void setSubject(SubjectEntity subject) {
+        this.subject = subject;
+    }
+
+    public List<StudentEntity> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<StudentEntity> students) {
+        this.students = students;
+    }
+
+    public List<ClassEntity> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<ClassEntity> classes) {
+        this.classes = classes;
+    }
 }
